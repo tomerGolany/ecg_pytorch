2852e938cb11292c17240ef383967608bd18102d
Tomer Golany <tomer.golany@gmail.com>
2019-06-30 21:03:57 +0300

    Added package files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['matplotlib', 'numpy']
+
+setup(name='ecg_pytorch',
+      version='0.1',
+      install_requires=REQUIRED_PACKAGES,
+      description='Deep learning methods for ECG classifications',
+      url='http://github.com/tomergolany/ecg_pytorch',
+      author='Tomer Golany',
+      author_email='tomer.golany@gmail.com',
+      license='Technion',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
