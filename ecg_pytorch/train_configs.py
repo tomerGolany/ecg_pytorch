b0dd3b1d0b3b44dd3375f881d941fa2071f7c879
Tomer Golany <tomer.golany@gmail.com>
2019-07-06 14:15:47 +0300

    Added device configurations

--- ecg_pytorch/train_configs.py
+++ ecg_pytorch/train_configs.py
@@ -1,3 +1,3 @@
 from collections import namedtuple
 
-ECGTrainConfig = namedtuple('ECGTrainConfig', 'num_epochs batch_size lr weighted_loss weighted_sampling')
+ECGTrainConfig = namedtuple('ECGTrainConfig', 'num_epochs batch_size lr weighted_loss weighted_sampling device')
