a8ad646568d46d0a9f455ee484db77b3be8c8de0
Tomer Golany <tomer.golany@gmail.com>
2019-07-06 14:30:37 +0300

    Added server path

--- ecg_pytorch/data_reader/pickle_data.py
+++ ecg_pytorch/data_reader/pickle_data.py
@@ -1,7 +1,7 @@
 import pickle
 
 full_path = '/Users/tomer.golany/PycharmProjects/ecg_pytorch/ecg_pytorch/data_reader'
-
+# full_path = '/home/tomer.golany@st.technion.ac.il/ecg_pytorch/ecg_pytorch/data_reader'
 
 def load_ecg_input_from_pickle():
     with open(full_path + '/train_beats.pickle', 'rb') as handle:
