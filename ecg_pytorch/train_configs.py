78e8e6a0b36386eea58bf54cc5a5b80f2b7c5d9f
Tomer Golany <tomer.golany@gmail.com>
2019-07-07 09:37:43 +0300

    Added generator configs

--- ecg_pytorch/train_configs.py
+++ ecg_pytorch/train_configs.py
@@ -1,3 +1,8 @@
 from collections import namedtuple
 
-ECGTrainConfig = namedtuple('ECGTrainConfig', 'num_epochs batch_size lr weighted_loss weighted_sampling device')
+ECGTrainConfig = namedtuple('ECGTrainConfig',
+                            'num_epochs batch_size lr weighted_loss weighted_sampling device add_data_'
+                            'from_gan generator_details')
+
+GeneratorAdditionalDataConfig = namedtuple('GeneratorAdditionalDataConfig', 'beat_type checkpoint_path num_examples_to_'
+                                                                            'add')
