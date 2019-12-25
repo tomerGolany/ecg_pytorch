c6e481149a45ab9c944eeb6b4fc940fbe5bb6dbb
Tomer Golany <tomer.golany@gmail.com>
2019-12-25 14:44:05 +0200

    config colab

--- ecg_pytorch/train_configs.py
+++ ecg_pytorch/train_configs.py
@@ -8,7 +8,8 @@ niv2_remote = '/home/tomer/'
 niv3_remote = '/media/drive/'
 yochai_remote = '/home/yochaiz/tomergolany/'
 google_remote = '/usr/local/google/home/tomergolany/'
-base = local_base
+colab_remote = '/content/'
+base = colab_remote
 
 ECGTrainConfig = namedtuple('ECGTrainConfig',
                             'num_epochs batch_size lr weighted_loss weighted_sampling device add_data_'
